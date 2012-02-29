b5408b0d9043f0271024946ec9818864df61e0c6
Danny Ayers <danny.ayers@gmail.com>
2012-02-29 08:05:41 +0100

    tweak

--- misc/router.js
+++ misc/router.js
@@ -4,9 +4,9 @@ var http = require('http'),
 var options = {
   router: {
 //	  'dannyayers.com': '127.0.0.1:8000',
-  'hyperdata.org': '67.207.128.128:80',	  
+ // 'hyperdata.org': '67.207.128.128:80',	  
  //   'pragmatron.org': '127.0.0.1:8000',
-    'dannyayers.com': '67.207.128.128:8080'
+    'semtext.org': '67.207.128.128:9000'
   }
 };
 
@@ -14,3 +14,9 @@ var proxyServer = httpProxy.createServer(options);
 
 
 proxyServer.listen("67.207.128.128", 8088);
+
+http.createServer(function (req, res) {
+	  res.writeHead(200, { 'Content-Type': 'text/plain' });
+	  res.write('request successfully proxied: ' + req.url +'\n' + JSON.stringify(req.headers, true, 2));
+	  res.end();
+	}).listen(9000); 
