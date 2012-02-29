b0efd149bbc2213de2fd7e7a3551199aa4defdba
Danny Ayers <danny.ayers@gmail.com>
2012-02-29 08:23:53 +0100

    tweak

--- misc/router.js
+++ misc/router.js
@@ -6,17 +6,17 @@ var options = {
 //	  'dannyayers.com': '127.0.0.1:8000',
  // 'hyperdata.org': '67.207.128.128:80',	  
  //   'pragmatron.org': '127.0.0.1:8000',
-    'semtext.org': '67.207.128.128:9000'
+    'semtext.org': '127.0.0.1:9000'
   }
 };
 
 var proxyServer = httpProxy.createServer(options);
 
 
-proxyServer.listen("67.207.128.128", 8888);
+proxyServer.listen(8888);
 
 http.createServer(function (req, res) {
 	  res.writeHead(200, { 'Content-Type': 'text/plain' });
 	  res.write('request successfully proxied: ' + req.url +'\n' + JSON.stringify(req.headers, true, 2));
 	  res.end();
-	}).listen('67.207.128.128', 9000); 
+	}).listen(9000); 
