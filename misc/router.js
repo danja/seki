5cfd0004a22e51b84c13c5bc250085038584b5b7
Danny Ayers <danny.ayers@gmail.com>
2012-02-29 08:53:16 +0100

    tweak

--- misc/router.js
+++ misc/router.js
@@ -13,10 +13,10 @@ var options = {
 var proxyServer = httpProxy.createServer(options);
 
 
-proxyServer.listen(8888);
+proxyServer.listen('67.207.128.128',8888);
 
 http.createServer(function (req, res) {
 	  res.writeHead(200, { 'Content-Type': 'text/plain' });
 	  res.write('request successfully proxied: ' + req.url +'\n' + JSON.stringify(req.headers, true, 2));
 	  res.end();
-	}).listen(9000); 
+	}).listen('67.207.128.128', 9000); 
