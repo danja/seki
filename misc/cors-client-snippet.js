ac5e577ea62af4d3125e4ba9e4fccac1b94300de
Danny Ayers <danny.ayers@gmail.com>
2013-05-23 22:22:40 +0200

    started adding vocabs

---
+++ misc/cors-client-snippet.js
@@ -0,0 +1,11 @@
+  var x = new XMLHttpRequest();
+  if ("withCredentials" in x) {
+    x.open("GET", o.url, true);
+  } else if (typeof XDomainRequest != "undefined") {
+    x = new XDomainRequest();
+    x.open("GET", o.url);
+  } else {
+    // Otherwise, CORS is not supported by the browser.
+    x = null;
+  }
+
