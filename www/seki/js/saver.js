dad58c38bbb354cc7a9028262851deaae612f01e
Danny Ayers <danny.ayers@gmail.com>
2013-05-26 10:50:32 +0200

    working on aloha

--- www/seki/js/saver.js
+++ www/seki/js/saver.js
@@ -1,32 +1,39 @@
 Aloha.ready(function() {
-	Aloha.require( ['aloha', 'aloha/jquery'], function( Aloha, jQuery) {
+	Aloha.require( ['aloha', 'aloha/jquery'], function( Aloha, $) { // $ was jQuery
 		
 		// save all changes after leaving an editable
 		Aloha.bind('aloha-editable-deactivated', function(){
 			var content = Aloha.activeEditable.getContents();
 			var contentId = Aloha.activeEditable.obj[0].id;
 			var pageId = window.location.pathname;
+			var uri = $('#uri').text();
+			
+			console.log("Content = "+content);
+			console.log("ContentID = "+contentId);
+			console.log("pageId = "+pageId);
+			console.log("uri = "+uri);
 			
 			// textarea handling -- html id is "xy" and will be "xy-aloha" for the aloha editable
 			if ( contentId.match(/-aloha$/gi) ) {
 				contentId = contentId.replace( /-aloha/gi, '' );
 			}
-
-			var request = jQuery.ajax({
+ 
+			var request = $.ajax({
 				url: "save.php",
 				type: "POST",
 				data: {
 					content : content,
 					contentId : contentId,
-					pageId : pageId
+					pageId : pageId,
+					uri : uri
 				},
 				dataType: "html"
 			});
-
+ 
 			request.done(function(msg) {
-				jQuery("#log").html( msg ).show().delay(800).fadeOut();
+				$("#log").html( msg ).show().delay(800).fadeOut();
 			});
-
+ 
 			request.error(function(jqXHR, textStatus) {
 				alert( "Request failed: " + textStatus );
 			});
