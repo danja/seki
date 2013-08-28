75d5156de242ff5d4f4b962370b7209b67a23329
Danny Ayers <danny.ayers@gmail.com>
2013-08-28 09:15:19 +0200

    changing ipaddress of node server

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var SampleApp = function() {
     self.setupVariables = function() {
         //  Set the environment variables we need.
         self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = 8888; // process.env.OPENSHIFT_NODEJS_PORT || 
+        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
         if (typeof self.ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
