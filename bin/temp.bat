f076ea267e04472b661c8968888215f90a61a786
Danny Ayers <danny.ayers@gmail.com>
2013-09-04 09:32:47 +0200

    openshift settings

---
+++ bin/temp.bat
@@ -0,0 +1,5 @@
+java -Xmx1200M -jar ../jena-fuseki-0.2.7/fuseki-server.jar --help
+# --host=$OPENSHIFT_NODEJS_IP 
+
+# java -Xmx1200M -jar fuseki-server.jar --update --desc tdb.ttl /seki
+# --verbose
