015c9c1860bcf08304c812deb0f805703ea1554b
Danny Ayers <danny.ayers@gmail.com>
2013-09-04 11:35:18 +0200

    openshift config now appears to work

--- bin/run-fuseki-openshift.bat
+++ bin/run-fuseki-openshift.bat
@@ -1,4 +1,4 @@
-java -Xmx1200M -jar ../jena-fuseki-0.2.7/fuseki-server.jar --jetty-config ../conf/jetty.xml --verbose --update --config seki-config.ttl --pages ../jena-fuseki-0.2.7/pages
+java -Xmx1200M -jar ../jena-fuseki-0.2.7/fuseki-server.jar --jetty-config ../config/jetty.xml --verbose --update --config ../config/seki-config.ttl --pages ../jena-fuseki-0.2.7/pages
 # --host=$OPENSHIFT_NODEJS_IP 
 # 
 # java -Xmx1200M -jar fuseki-server.jar --update --desc tdb.ttl /seki
--- bin/run-fuseki.bat
+++ bin/run-fuseki.bat
@@ -1,4 +1,4 @@
-java -Xmx1200M -jar ../jena-fuseki-0.2.7/fuseki-server.jar --verbose --update --config seki-config.ttl --pages ../jena-fuseki-0.2.7/pages
+java -Xmx1200M -jar ../jena-fuseki-0.2.7/fuseki-server.jar --verbose --update --config ../config/seki-config.ttl --pages ../jena-fuseki-0.2.7/pages
 # ww
 
 # java -Xmx1200M -jar fuseki-server.jar --update --desc tdb.ttl /seki
