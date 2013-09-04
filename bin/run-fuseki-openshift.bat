3031dd3a4d77cb4fd910f854e2824fee074a9c85
Danny Ayers <danny.ayers@gmail.com>
2013-09-04 09:15:02 +0200

    openshift settings

--- bin/run-fuseki-openshift.bat
+++ bin/run-fuseki-openshift.bat
@@ -1,5 +1,5 @@
-java -Xmx1200M -jar ../jena-fuseki-0.2.7/fuseki-server.jar --port=15000 --host=$OPENSHIFT_NODEJS_IP --verbose --update --config seki-config.ttl --pages ../jena-fuseki-0.2.7/pages
-# ww
+java -Xmx1200M -jar ../jena-fuseki-0.2.7/fuseki-server.jar --port=15000 --verbose --update --config seki-config.ttl --pages ../jena-fuseki-0.2.7/pages
+# --host=$OPENSHIFT_NODEJS_IP 
 
 # java -Xmx1200M -jar fuseki-server.jar --update --desc tdb.ttl /seki
 # --verbose
